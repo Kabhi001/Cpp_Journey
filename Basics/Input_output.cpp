c0a4c85e917b131bea748ff64881f69a69f64d17
Abhishek Kumar <kabhishek10266@gmail.com>
2025-08-06 21:16:13 +0530

    Add files via upload

---
+++ Basics/Input_output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    cout<<"Enter your first number: ";
+    cin>>a;
+    cout<<"Enter your Second number: ";
+    cin>>b;
+    cout<<"The Sum of the two number is: "<< a + b <<endl;
+    cout<<"The difference of the two number is: "<< a - b <<endl;
+    cout<<"The multiply of the two number is: "<< a * b <<endl;
+    cout<<"The division of the two number is: "<< a / b <<endl;
+    return 0;
+
+}
